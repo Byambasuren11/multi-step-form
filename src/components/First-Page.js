d43d2eab4100a210d96d42874eb3db0725ebbfeb
Byambasuren11 <165979583+Byambasuren11@users.noreply.github.com>
2025-01-22 13:06:14 +0800

    g

--- src/components/First-Page.js
+++ src/components/First-Page.js
@@ -1,40 +1,70 @@
 import { useState } from "react";
 
 const First = () => {
-  const [namesValue, setNamesValue] = useState([]);
+  const [firstNamesValue, setFirstNamesValue] = useState([]);
+  const [secondNamesValue, setSecondNamesValue] = useState([]);
+  const [userNamesValue, setUserNamesValue] = useState([]);
   const [error, setError] = useState();
-  const onClick = () => {};
-  const c=(e)=>{
-setNamesValue(e.target.value);
-  }
-  console.log(namesValue);
+  const onClick = () => {
+    if (firstNamesValue.length == 0) {
+      setError({ firstName: "Enter first name" });
+    }
+    if (secondNamesValue.length == 0) {
+      setError({ secondName: "Enter second name" });
+    }
+    if (userNamesValue.length == 0) {
+      setError({ userName: "Enter user name" });
+    }
+    setError("");
+  };
+  const firstNameChanged = (e) => {
+    setFirstNamesValue(e.target.value);
+  };
+  const secondNameChanged = (e) => {
+    setSecondNamesValue(e.target.value);
+  };
+  const userNameChanged = (e) => {
+    setUserNamesValue(e.target.value);
+  };
+  console.log(firstNamesValue);
   return (
     <>
       <div className="flex flex-col gap-10 mt-6">
         <div className=" flex flex-col gap-2">
           <p>First name</p>
           <input
-          value={namesValue}
-          onChange={c}
+            value={firstNamesValue}
+            onChange={firstNameChanged}
             className=" w-full p-3 border border-gray-500 rounded-lg"
             placeholder="Your first name"
           />
+          {error && error.firstName && (
+            <div className="text-red-500">{error.firstName}</div>
+          )}
         </div>
         <div className=" flex flex-col gap-2">
           <p>Last name</p>
           <input
-            value={namesValue}
-            onChange={c}
+            value={secondNamesValue}
+            onChange={secondNameChanged}
             className="w-full p-3 border border-gray-500 rounded-lg"
             placeholder="Your last name"
           />
+          {error && error.firstName && (
+            <div className="text-red-500">{error.secondName}</div>
+          )}
         </div>
         <div className=" flex flex-col gap-2">
           <p>User name</p>
           <input
+            value={userNamesValue}
+            onChange={userNameChanged}
             className="w-full p-3 border border-gray-500 rounded-lg"
             placeholder="Your username"
           />
+          {error && error.firstName && (
+            <div className="text-red-500">{error.userName}</div>
+          )}
         </div>
         <button
           className="px-5 py-3.5 bg-black text-white rounded-lg"
